e0c1db9344d57a7697cfb70603e8e7b180bb6f60
Ivansquark <squark@mail.ru>
2020-08-31 13:06:06 +0300

    +bootloader on pressed button

--- src/main.cpp
+++ src/main.cpp
@@ -8,51 +8,51 @@ void *__dso_handle = nullptr; // dummy "guard" that is used to identify dynamic 
 
 static constexpr uint32_t last_page= 0x0803F800; // - address of last flash 2k page (2k=0x800, 1k=0x400)
 
-#pragma pack(push,1)
-typedef struct 
-{
-	uint8_t x;
-	uint8_t y;
-	uint16_t z;
-}buf;
-#pragma pack(pop)
-
 SCSI scsi;
 Flash flash;
 QueT<uint8_t,512> que;
 
-	
+bool BootState()
+{	
+	RCC->APB2ENR|=RCC_APB2ENR_IOPAEN;
+	return GPIOA->IDR|=GPIO_IDR_IDR0; //если есть питание на пине
+}	
 
 int main()
-{		
-    RCCini rcc;	//! 72 MHz	
-	RCC->APB2ENR|=RCC_APB2ENR_IOPBEN;
-	RCC->APB2ENR|=RCC_APB2ENR_AFIOEN;
-	AFIO->MAPR|=AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
-	SpiLcd lcd;
-	lcd.fillScreen(0xff00);
-	Font_16x16 font16;
-	USB_DEVICE usb;
-	__enable_irq();
-	USART_debug usart2(2);
-	//uint8_t arr[4]={0x01,0x00,0x00,0x00};	
-	//buf b = {0,0,1};
-	//flash.write_any_buf(last_page,&b,4);
-	while(1)
-	{
-		//flash_read_buf(last_pagёe,&b,4);
-		//font16.intToChar(b.z);
-		//font16.print(10,10,0x00ff,font16.arr,2);		
-		if(scsi.recieveCommandFlag)
+{		    
+	if (BootState)
+	{//! входим в bootloader
+		RCCini rcc;	//! 72 MHz	
+		RCC->APB2ENR|=RCC_APB2ENR_IOPBEN;
+		RCC->APB2ENR|=RCC_APB2ENR_AFIOEN;
+		AFIO->MAPR|=AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
+		SpiLcd lcd;
+		lcd.fillScreen(0xff00);
+		Font_16x16 font16;
+		USB_DEVICE usb;
+
+		__enable_irq();
+		USART_debug usart2(2);
+		while(1)
 		{
-			//USART_debug::usart2_sendSTR("\n Execute \n");
-			scsi.SCSI_Execute(2); //обработка протокола SCSI лежащего в приемном буффере устройства	
-			
-		}	
-		font16.intToChar(USB_DEVICE::pThis->resetFlag);
-		font16.print(10,100,0x00ff,font16.arr,3);
-		font16.intToChar(USB_DEVICE::pThis->counter);
-		font16.print(100,100,0x00ff,font16.arr,3);
+			if(scsi.recieveCommandFlag)
+			{
+				//USART_debug::usart2_sendSTR("\n Execute \n");
+				scsi.SCSI_Execute(2); //обработка протокола SCSI лежащего в приемном буффере устройства				
+			}	
+			font16.intToChar(USB_DEVICE::pThis->resetFlag);
+			font16.print(10,100,0x00ff,font16.arr,3);
+			font16.intToChar(USB_DEVICE::pThis->counter);
+			font16.print(100,100,0x00ff,font16.arr,3);
+		}
 	}
+	else //! идем в код с адреса 0x08005000
+	{
+		//NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x08005000); //смещаем таблицу векторов прерываний на новый адрес 
+		flash.goToUserApp();
+		/*!< необходимо в скрипте линкера новой прошивки указывать 
+			 адрес старта программы (секция кода ROM или FLASH) с 0x08005000
+			 начиная с этого адреса и будет располагаться таблица векторов прерывания>*/
+	}	
     return 0;
 }
